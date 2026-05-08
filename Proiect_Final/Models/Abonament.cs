de63bf0d0a3f7be045860777e87e9d30fe635d1d
P3te07 <petruodobescu@gmail.com>
2026-05-08 17:42:17 +0300

    Definirea modelelor pentru entitati #10

---
+++ Proiect_Final/Models/Abonament.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proiect_Final.Models
+{
+    public class Abonament
+    {
+        public int IdAbonament { get; set; }
+        public string Tip { get; set; }
+        public decimal Pret { get; set; }
+        public int GradAcces { get; set; }
+
+    }
+}
---
+++ Proiect_Final/Models/Inregistrari.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proiect_Final.Models
+{
+    public class Inregistrari
+    {
+        public int Id { get; set; }
+        public int IdClient { get; set; }
+        public int IdAbonament { get; set; }
+        public DateTime DataStart { get; set; }
+        public DateTime DataFinish { get; set; }
+    }
+}
